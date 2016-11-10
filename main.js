99a98c994f41effb6ddc86a34d4d19ea73357ce5
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-11-10 11:26:14 -0800

    bundling

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow () {
   win.loadURL(`https://quizzly-client-prod.herokuapp.com/`)
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
