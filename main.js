051eecdee193bcd248ec28ce3daee914cc286804
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-11-10 11:28:03 -0800

    Ready for bundling

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(`http://localhost:4040/index.html`)
+  win.loadURL(`https://quizzly-client-prod.herokuapp.com/`)
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
